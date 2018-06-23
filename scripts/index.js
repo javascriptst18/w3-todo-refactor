72a3c8cb756a233437595cebd6cf5754e1767c17
Jesper Orb <jesper.orb@zocom.se>
2018-06-23 08:23:20 +0200

    for..of instead of for(start, condition, increase)

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,20 @@
 // Create a "close" button and append it to each list item
 function createCloseButtons() {
   var myNodelist = document.getElementsByTagName("LI");
-  var i;
-  for (i = 0; i < myNodelist.length; i++) {
+  for (let listItemElement of myNodelist) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
     span.appendChild(txt);
-    myNodelist[i].appendChild(span);
+    listItemElement.appendChild(span);
   }
 }
 
 // Click on a close button to hide the current list item
 function bindCloseButtonEvents() {
   var close = document.getElementsByClassName("close");
-  var i;
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
+  for (let closeButton of close) {
+    closeButton.onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     }
@@ -56,8 +54,8 @@ function newElement() {
   span.appendChild(txt);
   li.appendChild(span);
 
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function () {
+  for (let closeButton of close) {
+    closeButton.onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     }
